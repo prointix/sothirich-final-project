5fcd2a440c0a37d8446ae477c716cbbde0ae0fc8
Sothirich <peangsereysothirich@gmail.com>
2022-09-06 11:55:05 +0700

    structure and navigation

---
+++ app/screens/Profile/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Profile;
---
+++ app/screens/SignOut/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const SignOut = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignOut;
